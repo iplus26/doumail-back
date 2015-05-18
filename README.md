5ca4a265d85d7a0464887ce279a55b7f6d9e0f61
iplus26 <iplus26@gmail.com>
2015-05-18 15:37:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# 概述
+
+将「私信」替换成「豆邮」的 Safari / Chrome 浏览器拓展
+
+![截图](https://github.com/iplus26/doumail-back-safari/blob/master/Screenshot.png)
+
+# 背景
+
+人人尸骨未寒，豆瓣继续作死。
+
+[如何评价 2013 年 5 月豆瓣「豆邮」的改版？- 知乎](http://www.zhihu.com/question/21123333)
+
+[还我豆邮！！！- 豆瓣线上活动](http://www.douban.com/online/12214968/)
+
+# 安装
+
+## Safari
+
+下载 doumail-back.safariextz 双击安装。
+
+下载地址: https://raw.githubusercontent.com/iplus26/doumail-back-safari/master/doumail-back.safariextz
+
+## Chrome
+
+下载 doumail-back-chrome.crx 双击安装。
+
+下课回去提交 Chrome 商店，好像现在 Windows 下非官网的插件不能安装。[Chrome 封杀所有非官方市场插件 - 36氪](http://36kr.com/p/532872.html)
+
+# 联系我
+
+有任何问题欢迎发邮件至 iplus26@gmail.com 或者微信我 ivanfd.
---
+++ doumail-back-chrome/doumail-back.js
@@ -0,0 +1,33 @@
+console.log("还我豆邮!  -- Ivan");
+
+// 标题
+document.title = document.title.replace(/私信/g, "我的豆邮");
+
+// 导航栏
+var p1 = document.querySelector("#db-global-nav");
+p1.innerHTML = p1.innerHTML.replace(/私信/g, "豆邮");
+
+if(location.pathname.indexOf("people")  != -1) {
+	// 发私信
+	var p5 = document.querySelector(".user-opt");
+	p5.innerHTML = p5.innerHTML.replace(/私信/g, "豆邮");
+}
+
+
+if(location.pathname.indexOf("doumail") != -1){
+	// 豆邮页面
+	document.querySelector("#content h1").innerHTML = "我的豆邮";
+
+	// 右侧边栏
+	var p3 = document.querySelector(".aside");
+	p3.innerHTML = p3.innerHTML.replace(/私信/g, "豆邮");
+
+	// tabs
+	var p4 = document.querySelector(".doumail-toolbar");
+	if (p4 != null) {
+		p4.innerHTML = p4.innerHTML.replace(/私信/g, "豆邮");
+	}
+		
+	// 「回应」按钮旁边的广告
+	document.querySelector(".item-submit").innerHTML = '<span class="bn-flat"><input type="submit" name="m_reply" value="回应"></span>';
+}
